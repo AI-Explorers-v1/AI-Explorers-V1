c5222e25cf05c871d26788bf65c72513441cd52b
Eric Liu <25liue03@stu.smuhsd.org>
2023-11-15 18:50:52 -0800

    fileextract

---
+++ src/app/aboutus/page.js
@@ -0,0 +1,12 @@
+import Nav from "@/app/components/navbar"
+import Footer from "@/app/components/footer"
+
+export default function AboutUs() {
+    return (
+        <main>
+            <Nav />
+            <h1 class="font-semibold text-title text-center text-blue-700"> About Us</h1>
+            <Footer />
+        </main>
+    )
+}
