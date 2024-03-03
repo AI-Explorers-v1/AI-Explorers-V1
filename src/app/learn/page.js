235d846004e9d90ba83747b62259a7857be5cb5e
Eric Liu <25liue03@stu.smuhsd.org>
2024-03-03 13:30:20 -0800

    t

--- src/app/learn/page.js
+++ src/app/learn/page.js
@@ -7,12 +7,14 @@ export default function Learn() {
         <main>
             <SideNavbar />
             <Nav />
+        <div className="learn-page">
             <div className="flex">
-                
-                <div className="flex-1">
-                    <h1 className="font-semibold text-title text-center text-blue-700">Learn</h1>
-                    {/* Add your Learn content here */}
-                </div>
+                    <div className="flex-1">
+                        <h1 className="font-semibold text-title text-center text-blue-700">Learn</h1>
+                        {/* Add your Learn content here */}
+                        <p>This is your Learn page content.</p>
+                    </div>
+            </div>
             </div>
             <Footer />
         </main>
