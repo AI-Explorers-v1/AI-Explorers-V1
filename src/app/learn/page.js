d851bfbe7232bbe98088060e48448b05cceca667
Eric Liu <25liue03@stu.smuhsd.org>
2024-03-03 10:20:33 -0800

    learn

--- src/app/learn/page.js
+++ src/app/learn/page.js
@@ -1,11 +1,11 @@
 import Nav from "@/app/components/navbar"
 import Footer from "@/app/components/footer"
 
-export default function Events() {
+export default function learn() {
     return (
         <main>
             <Nav />
-            <h1 class="font-semibold text-title text-center text-blue-700"> Events</h1>
+            <h1 class="font-semibold text-title text-center text-blue-700"> Learn</h1>
             <Footer />
         </main>
     )
